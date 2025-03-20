cf6d13d35b0822aeb70463534e1a896b21b95bb8
naolumer <naolumer3@gmail.com>
2025-03-20 15:09:51 +0300

    removes maxAge option from logout controller cookie options

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -98,7 +98,6 @@ export const logout = async (req,res)=> {
             httpOnly:true,
             secure : process.env.NODE_ENV === "production",
             sameSite: process.env.NODE_ENV ==="production"? "none":"strict",
-            maxAge: 7*24*60*60*1000
         })
 
         return res.json({
