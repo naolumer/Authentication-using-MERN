ec3722c4a39e457779a20424416837566c08ea77
naolumer <naolumer3@gmail.com>
2025-03-20 15:06:40 +0300

    login and logout controllers

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -43,3 +43,70 @@ export const register = async(req,res)=>{
         res.json({success:false, message:error.message})
     }
 }
+
+export const login = async (req,res)=> {
+
+    const {email,password} = req.body
+
+    if (!email || !password){
+        return res.json({
+            success:false,
+            message:"Email and password required"
+        })
+    }
+    try {
+        const user = await userModel.findOne({email})
+
+        if (!user){
+            return res.json({
+                success:false,
+                message:"Invalid Email"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch){
+            return res.json({
+                success:false,
+                message:"Invalid Password"
+            })
+        }
+
+        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET,{expiresIn: "7d"})
+        res.cookie("token",token,{
+            httpOnly:true,
+            secure : process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV ==="production"? "none":"strict",
+            maxAge: 7*24*60*60*1000
+        })
+
+        return res.status(200).json({
+            success:true,
+            message:"Login successful!"
+        })
+
+    } catch(error) {
+        res.json({success:false, message:error.message})
+    }
+}
+
+export const logout = async (req,res)=> {
+
+    try {
+        res.clearCookie("token",{
+            httpOnly:true,
+            secure : process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV ==="production"? "none":"strict",
+            maxAge: 7*24*60*60*1000
+        })
+
+        return res.json({
+            success:true,
+            message:"Logout was successful"
+        })
+
+    } catch(error){
+        res.json({success:false, message:error.message})
+    }
+}
--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { register } from '../controllers/authController.js'
+import { login, logout, register } from '../controllers/authController.js'
 
 const authRouter = express.Router()
 
 
 authRouter.post("/register",register)
+authRouter.post("/login", login)
+authRouter.post("/logout", logout)
 
 export default authRouter
