ab230ce166a766fe187c0c6a83213618d68dd878
naolumer <naolumer3@gmail.com>
2025-03-22 14:18:30 +0300

    Home page ui

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
           alt="Logo"
         />
         <div className="flex items-center gap-2 border border-gray-400 rounded-full py-2 px-6 hover:bg-gray-50 hover:border-indigo-500 cursor-pointer">
-          <p className="text-lg font-semibold text-indigo-900">Login</p>
+          <p onClick={()=>navigate("/login")} className="text-lg font-semibold text-indigo-900">Login</p>
           <img className="font-bold" src={assets.arrow_icon} alt="Arrow Icon" />
         </div>
       </div>
