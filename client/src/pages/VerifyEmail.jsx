6da9bd5335912a3f364d86b17d34a1b92046c763
naolumer <naolumer3@gmail.com>
2025-03-24 14:00:26 +0300

    paste otp functionality

--- client/src/pages/VerifyEmail.jsx
+++ client/src/pages/VerifyEmail.jsx
@@ -10,7 +10,26 @@ import { useContext } from 'react'
 
 const VerifyEmail = () => {
 
+  const {backURL,isLoggedIn, userData,setUserData} = useContext(AppContext)
+  const navigate = useNavigate()
+
   const inputRefs = React.useRef([])
+
+  const onSubmitHandler = async(e)=>{
+    e.preventDefault()
+
+    const otpArray = inputRefs.current.map(e=>e.target.value)
+    const otpString = otpArray.join("")
+
+    axios.defaults.withCredentials = true
+    try {
+      const data = await axios.post(`{}`) 
+
+    } catch(error){
+      toast.error(error.message)
+    }
+  }
+
 
   const handleInput = (e,index)=>{
     if(e.target.value.length>0 && index < inputRefs.current.length-1){
@@ -24,6 +43,16 @@ const VerifyEmail = () => {
     }
   }
 
+  const handlePaste = (e,index)=>{
+    const paste = e.clipboardData.getData("text")
+    const pasteArray = paste.split("") 
+    pasteArray.forEach((char,index)=>{
+      if(inputRefs.current[index]){
+        inputRefs.current[index].value = char
+      }
+    })
+  }
+
   return (
     <div className='relative bg-gradient-to-br from-blue-200 to-purple-400 min-h-screen w-full flex items-center justify-center'>
       <div onClick={()=>navigate("/")} className='absolute top-5 left-5 width-full ml-10 cursor-pointer'>
@@ -32,11 +61,11 @@ const VerifyEmail = () => {
 
       
         <div className='w-[430px] sm:w-[460px] md:w-[500px] '>
-        <form className='bg-slate-900 w-full rounded-lg h-fit flex flex-col items-center gap-6 py-10 px-6  ' >
+        <form onSubmit={onSubmitHandler} className='bg-slate-900 w-full rounded-lg h-fit flex flex-col items-center gap-6 py-10 px-6  ' >
           <h1 className='text-2xl sm:text-3xl font-bold mt-4 text-white'>Email Verification OTP</h1>
           <p className='text-indigo-400'>Enter the 6-digit code sent to your email.</p>
 
-          <div className='flex justify-between mb-8 w-[85%]'>
+          <div className='flex justify-between mb-8 w-[85%]' onPaste={handlePaste}>
             {
               Array(6).fill(0).map((_,index)=>(
                 <input className='text-white text-center rounded-md text-xl bg-[#333A5C] w-12 h-12'
@@ -50,7 +79,7 @@ const VerifyEmail = () => {
               ))
             }
           </div>
-          <button className='w-[85%] text-lg py-2 md:py-3 bg-gradient-to-r  from-indigo-500 to-indigo-800 hover:bg-gradient-to-tl hover:from-indigo-800 hover:to-indigo-500 rounded-full text-white mb-6 '>Verify Email</button>
+          <button type='submit' className='w-[85%] text-lg py-2 md:py-3 bg-gradient-to-r  from-indigo-500 to-indigo-800 hover:bg-gradient-to-tl hover:from-indigo-800 hover:to-indigo-500 rounded-full text-white mb-6 '>Verify Email</button>
           </form>
         </div>   
     </div>
