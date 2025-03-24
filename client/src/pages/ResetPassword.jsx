b5b004fe38e5d1749c75a7f1488d1ca895bd8932
naolumer <naolumer3@gmail.com>
2025-03-24 16:47:30 +0300

    conditionally render the input fields

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,16 +1,63 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
-// import { useState } from 'react'
-// import { AppContext } from '../context/AppContext'
-// import axios from "axios"
-// import {toast} from "react-toastify"
-// import { useContext } from 'react'
+
+import { AppContext } from '../context/AppContext'
+import axios from "axios"
+import {toast} from "react-toastify"
+import { useContext } from 'react'
 
 const ResetPassword = () => {
   const navigate = useNavigate()
-
+  const {backURL} = useContext(AppContext)
   const inputRefs = useRef([])
+
+  const [email,setEmail] = useState("")
+  const [emailSent, setEmailSent] = useState(false)
+  const [otp, setOtp] = useState(0)
+  const [isOtpSet,setIsOtpSet] = useState(false)
+  const [newPassword, setNewPassword] = useState("")
+
+  axios.defaults.withCredentials = true
+
+  const onSubmitEmail = async (e)=>{
+    e.preventDefault()
+
+    try {
+      const {data} = await axios.post(`${backURL}/api/auth/send-resetotp`,{email})
+      toast.success(data.message)
+      data.success && setEmailSent(true)
+    } catch(error){
+      toast.error(error.message)
+    }
+  }
+
+  const onSubmitOtp = (e)=>{
+    e.preventDefault()
+    const otpArray = inputRefs.current.map(e => e.value)
+    const otpString = otpArray.join("")
+    setOtp(otpString)
+    setIsOtpSet(true)
+  }
+
+  const onSubmitNewPassword = async (e)=>{
+    e.preventDefault()
+
+    try {
+      const {data} = await axios.post(`${backURL}/api/auth/reset-password`,{
+      email,
+      otp,
+      newPassword
+    })
+
+    data.success && toast.success(data.message)
+    data.success && navigate("/login")
+
+    } catch(error){
+      toast.error(error.message)
+    }
+    
+  }
 
   const handleInput = (e,index)=>{
     if(e.target.value.length>0 && index < inputRefs.current.length-1){
@@ -41,9 +88,10 @@ const ResetPassword = () => {
       </div>
 
       {/* Email input section */}
-        
-        <div className='w-[320px] sm:w-[350px] md:w-[370px]'>
-        <form  className='bg-slate-900 w-full rounded-lg h-fit flex flex-col items-center gap-3 py-6 px-4  ' >
+
+      {!emailSent && 
+      <div className='w-[320px] sm:w-[350px] md:w-[370px]'>
+        <form onSubmit={onSubmitEmail}  className='bg-slate-900 w-full rounded-lg h-fit flex flex-col items-center gap-3 py-6 px-4  ' >
           <h1 className='text-2xl sm:text-3xl font-bold mt-4 text-white'>Reset Password</h1>
           <p className=' text-indigo-200 mb-2 sm:mb-3'>Enter your registerd email address.</p>
     
@@ -54,17 +102,24 @@ const ResetPassword = () => {
             rounded-full bg-transparent font-light text-white placeholder:text-gray-400 placeholder:text-sm placeholder:font-light w-full' 
               type="email" 
               placeholder='Email id'
-              required/>
+              required
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)}/>
           </div>
           <button type='submit' className='w-[85%] text-lg py-2 md:py-2 bg-gradient-to-r  from-indigo-500 to-indigo-800 
           hover:bg-gradient-to-tl hover:from-indigo-800 hover:to-indigo-500 rounded-full text-white mb-6'>Submit</button>
           </form>
         </div> 
+      }
+        
+        
 
         {/* Password reset otp section */}
 
+        {emailSent && !isOtpSet && 
+        
         <div className='w-[370px] sm:w-[400px] md:w-[440px] '>
-        <form  className='bg-slate-900 w-full rounded-lg h-fit flex flex-col items-center gap-6 py-6 px-6  ' >
+        <form onSubmit={onSubmitOtp} className='bg-slate-900 w-full rounded-lg h-fit flex flex-col items-center gap-6 py-6 px-6  ' >
           <h1 className='text-2xl sm:text-3xl font-bold mt-4 text-white'>Password Reset OTP</h1>
           <p className='text-indigo-400'>Enter the 6-digit code sent to your email.</p>
 
@@ -85,12 +140,16 @@ const ResetPassword = () => {
           <button type='submit' className='w-[85%] text-lg py-2 md:py-2 bg-gradient-to-r  from-indigo-500 to-indigo-800 
           hover:bg-gradient-to-tl hover:from-indigo-800 hover:to-indigo-500 rounded-full text-white mb-4 '>Submit</button>
           </form>
-        </div>  
+        </div>  }
+
+        
 
             {/* New Password Input */}
-
+        
+        {emailSent && isOtpSet && 
+        
         <div className='w-[320px] sm:w-[350px] md:w-[370px]'>
-          <form  className='bg-slate-900 w-full rounded-lg h-fit flex flex-col items-center gap-3 py-6 px-3  ' >
+          <form onSubmit={onSubmitNewPassword}  className='bg-slate-900 w-full rounded-lg h-fit flex flex-col items-center gap-3 py-6 px-3  ' >
             <h1 className='text-2xl sm:text-3xl font-bold mt-4 text-white'>Enter Password</h1>
             <p className=' text-indigo-200 mb-2 sm:mb-3'>Enter your new password</p>
               
@@ -101,13 +160,18 @@ const ResetPassword = () => {
               rounded-full bg-transparent placeholder:text-gray-400 placeholder:text-sm placeholder:font-light w-full' 
                 type="password" 
                 placeholder='Password'
-                required/>
+                required
+                value={newPassword}
+                onChange={(e)=>setNewPassword(e.target.value)}/>
             </div>
             <button type='submit' className='w-[85%] text-lg py-1 md:py-2 bg-gradient-to-r  
             from-indigo-500 to-indigo-800 hover:bg-gradient-to-tl 
             hover:from-indigo-800 hover:to-indigo-500 rounded-full text-white mb-6'>Reset password</button>
           </form>
         </div>   
+        }
+
+        
     </div>
   )
 }
