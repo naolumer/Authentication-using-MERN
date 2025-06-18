2758e184ff79c0be66878a6b0cad1bd48da92846
naolumer <naolumer3@gmail.com>
2025-06-18 18:16:20 +0300

    removed unnecessary imports

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import  { useRef } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
